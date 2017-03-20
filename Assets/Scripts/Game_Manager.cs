50a5c3f113b6ed43ca847ef196144131cd69e8ff
danibq <dani_bq2@hotmail.com>
2017-03-20 11:51:42 +0000

    Initial Project Upload
    Initial Project Upload

---
+++ Assets/Scripts/Game_Manager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game_Manager : MonoBehaviour
+{
+    private bool Paused;
+    public GameObject PauseTitle;
+    public GameObject ResumeButton;
+    public GameObject RestartButton;
+    public GameObject QuitButton;
+
+	void Start ()
+    {
+        Time.timeScale = 1.0f;
+        Paused = false;
+	}
+	
+	void Update ()
+    {
+	    
+	}
+
+    public void Pause ()
+    {
+        Paused = !Paused;
+
+        if (!Paused)
+        {
+            Time.timeScale = 1.0f;
+
+            PauseTitle.SetActive(false);
+            ResumeButton.SetActive(false);
+            RestartButton.SetActive(false);
+            QuitButton.SetActive(false);
+        }
+
+        if(Paused)
+        {
+            Time.timeScale = 0.0f;
+
+            PauseTitle.SetActive(true);
+            ResumeButton.SetActive(true);
+            RestartButton.SetActive(true);
+            QuitButton.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Scene_navigation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_navigation : MonoBehaviour {
+
+	public void Game()
+	{
+		Application.LoadLevel ("Game_scene");
+	}
+
+	public void Highscore()
+	{
+		Application.LoadLevel ("HighScore_scene");
+	}
+
+	public void Menu()
+	{
+		Application.LoadLevel ("Menu_scene");
+	}
+
+	public void Options()
+	{
+		Application.LoadLevel ("Options_scene");
+	}
+
+    
+
+}
